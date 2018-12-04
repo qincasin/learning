84ec676b0fdf02936a19116494afa0b4a9e07557
qincasin <qinjiaxing@originaltek.com>
2018-12-04 21:32:39 +0800

    SimpleDataFormat和JodaTime

---
+++ concurrency-demo/src/main/java/com/qjx/concurrency/example/commonunsafe/DateFormatExample1.java
@@ -0,0 +1,64 @@
+package com.qjx.concurrency.example.commonunsafe;
+
+import com.qjx.concurrency.annotations.NotThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.text.SimpleDateFormat;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+/**
+ * DateFormat
+ */
+@Slf4j
+@NotThreadSafe
+public class DateFormatExample1 {
+
+    private static SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMdd");
+
+    /**
+     * 请求总数
+     */
+    private static int clientTotal = 5000;
+    /**
+     * 同事并发执行的线程数
+     */
+    private static int threadTotal = 200;
+
+
+    public static void main(String[] args) {
+
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+
+        for (int i = 0; i < clientTotal; i++) {
+            executorService.execute(()->{
+
+                try {
+                    semaphore.acquire();
+                    update();
+                    semaphore.release();
+                }catch (Exception e){
+                    log.error(e.getMessage(),e);
+                }
+            });
+        }
+        executorService.shutdown();
+
+
+    }
+
+    private static void update() {
+        try {
+            simpleDateFormat.parse("20181011");
+        }catch (Exception e){
+            log.error("parse exception",e);
+        }
+    }
+
+
+}
---
+++ concurrency-demo/src/main/java/com/qjx/concurrency/example/commonunsafe/DateFormatExample3.java
@@ -0,0 +1,64 @@
+package com.qjx.concurrency.example.commonunsafe;
+
+import com.qjx.concurrency.annotations.NotThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+import java.text.SimpleDateFormat;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+/**
+ * DateFormat
+ */
+@Slf4j
+@NotThreadSafe
+public class DateFormatExample3 {
+
+
+    /**
+     * 请求总数
+     */
+    private static int clientTotal = 5000;
+    /**
+     * 同事并发执行的线程数
+     */
+    private static int threadTotal = 200;
+
+    private static DateTimeFormatter dateTimeFormatter = DateTimeFormat.forPattern("yyyyMMdd");
+
+    public static void main(String[] args) {
+
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+
+        for (int i = 0; i < clientTotal; i++) {
+            final int count = i;
+            executorService.execute(()->{
+
+                try {
+                    semaphore.acquire();
+                    update(count);
+                    semaphore.release();
+                }catch (Exception e){
+                    log.error(e.getMessage(),e);
+                }
+            });
+        }
+        executorService.shutdown();
+
+
+    }
+
+    private static void update(int i) {
+        log.info("{}, {} ",i,DateTime.parse("20180101",dateTimeFormatter).toDate());
+    }
+
+
+}
