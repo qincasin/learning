81a08f7da26176b8a13a7764f45171955bb282f9
qincasin <qinjiaxing@originaltek.com>
2018-12-16 21:26:15 +0800

    feat: design-pattern
    设计模式

---
+++ design-pattern-demo/src/main/java/com/qjx/design/DesignPatternDemoApplication.java
@@ -0,0 +1,14 @@
+package com.qjx.design;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignPatternDemoApplication.class, args);
+    }
+
+}
+
---
+++ design-pattern-demo/src/test/java/com/qjx/design/DesignPatternDemoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.qjx.design;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DesignPatternDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
